3d80c56e4b533e5b448224de8643fc02b31774f7
AfrahSaud36 <afrahsaud36@gmail.com>
2024-10-27 21:57:05 +0300

    Initial Commit

---
+++ HydrateTests/HydrateTests.swift
@@ -0,0 +1,17 @@
+//
+//  HydrateTests.swift
+//  HydrateTests
+//
+//  Created by Afrah Alharbi on 24/04/1446 AH.
+//
+
+import Testing
+@testable import Hydrate
+
+struct HydrateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
